c286895c36aca35261536bcffc2bcf4102a3517b
Dhruv0802 <56443077+Dhruv0802@users.noreply.github.com>
2019-10-12 10:46:36 +0530

    Propose String_Length.java

---
+++ String Length.java
@@ -0,0 +1,7 @@
+class length{
+ void length_string{
+String name="Dhruv Sharma";
+int nameLength = name.length();
+System.out.println("The name " + name + " contains " + nameLength + "letters.");
+}
+}
