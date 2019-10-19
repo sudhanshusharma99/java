5ff84781a196a5b31371a59b0e0b8393de0d2d64
sudhanshusharma99 <56498080+sudhanshusharma99@users.noreply.github.com>
2019-10-19 16:05:39 +0530

    Update String Length.java

--- String Length.java
+++ String Length.java
@@ -5,3 +5,11 @@ int nameLength = name.length();
 System.out.println("The name " + name + " contains " + nameLength + "letters.");
 }
 }
+public class String {
+
+   public static void main(String args[]) {
+      char[] helloArray = { 'h', 'e', 'l', 'l', 'o', '.' };
+      String helloString = new String(helloArray);  
+      System.out.println( helloString );
+   }
+}
